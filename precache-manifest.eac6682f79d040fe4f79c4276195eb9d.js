c15290fd96a6e322f929a46805d9b19d1ac62486
Mudassir Sayed <mudassir@nimapinfotech.com>
2021-03-04 12:09:54 +0530

    Updates

---
+++ precache-manifest.eac6682f79d040fe4f79c4276195eb9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbac2ef456dbfabfa5c1477f3ffe7c4b",
+    "url": "/currency-search/index.html"
+  },
+  {
+    "revision": "803b6aecd9acdd27c974",
+    "url": "/currency-search/static/css/main.3fe2f957.chunk.css"
+  },
+  {
+    "revision": "e83d7e1709342028231e",
+    "url": "/currency-search/static/js/2.08afeb69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/currency-search/static/js/2.08afeb69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "803b6aecd9acdd27c974",
+    "url": "/currency-search/static/js/main.46551192.chunk.js"
+  },
+  {
+    "revision": "9c6afe79c32268ea0539",
+    "url": "/currency-search/static/js/runtime-main.b851f13e.js"
+  }
+]);
